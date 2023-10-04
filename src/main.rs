fcd6b6d2e45f5b464e68929a1e8dc70da47ee3c0
Rene van der Meer <rene@golemparts.com>
2023-10-04 02:54:49 +0200

    Print details for each GPIO line

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![allow(dead_code)]
 
 use std::error::Error;
-use std::fs::OpenOptions;
 use std::os::unix::io::AsRawFd;
 
 #[macro_use]
@@ -13,16 +12,43 @@ mod ioctl;
 use crate::ioctl::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Detect any gpiochip devices, and display their details
-    for id in 0..=255 {
-        if let Ok(file) = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .open(format!("{}{}", PATH_GPIOCHIP, id))
-        {
-            let chip_info = ChipInfo::new(file.as_raw_fd())?;
-            println!("{:?}", chip_info);
-        };
+    let gpiochip = find_gpiochip()?;
+    let chip_info = ChipInfo::new(gpiochip.as_raw_fd())?;
+
+    println!(
+        "ChipInfo: {:?} {:?} {}",
+        cbuf_to_cstring(&chip_info.name),
+        cbuf_to_cstring(&chip_info.label),
+        chip_info.lines
+    );
+
+    for line in 0..=chip_info.lines.min(MAX_OFFSET) {
+        if let Ok(line_info) = LineInfo::new(gpiochip.as_raw_fd(), line) {
+            // /            if let Ok(line_request) = LineRequest::new(gpiochip.as_raw_fd(), line) {
+            //                 let line_values = line_request.levels()?;
+
+            //                 println!(
+            //                     "{}: {} Flags:[{}] {} Level:[{}]",
+            //                     line_info.offset,
+            //                     cbuf_to_string(&line_info.name),
+            //                     line_info.flags(),
+            //                     line_info.flags,
+            //                     if (line_values.bits & 0x01) > 0 {
+            //                         "High"
+            //                     } else {
+            //                         "Low"
+            //                     },
+            //                 );
+            //             } else {
+            println!(
+                "{}: {} Consumer:[{}] Flags:[{}]",
+                line_info.offset,
+                cbuf_to_string(&line_info.name),
+                cbuf_to_string(&line_info.consumer),
+                line_info.flags(),
+            );
+            // }
+        }
     }
 
     Ok(())
